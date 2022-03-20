74b0b049e1560e5534854e97bb2bbe765bede7e8
AustinFoxtrot <98627923+AustinFoxtrot@users.noreply.github.com>
2022-03-19 22:05:00 -0500

    Delete Reader2.py

--- Reader2.py
+++
@@ -1,9 +0,0 @@
-#Purpose is to see if I could extract the text from a document I manually edited, save in a .xml and then retrieve that text from the .xml and display in a new word doc
-import lxml
-from docx import Document
-
-doc = Document('New.xml')
-new = Document()
-for paragraph in doc.paragraphs:
-    new.add_paragraph(paragraph.text)
-new.save('New.docx')
