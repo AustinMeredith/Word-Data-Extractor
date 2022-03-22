9de001180f3ef59601356b0a9030e2f34a72d137
AustinFoxtrot <98627923+AustinFoxtrot@users.noreply.github.com>
2022-03-21 19:13:24 -0500

    This is the current help class

--- Helpv3.py
+++ Helpv3.py
@@ -2,23 +2,23 @@ import os
 
 class Help:    #Help class declaration
         def HelpDisplay(self):
-            if(HelpDisplay=="help"):    #if the input is just help, display following message
+            if(HelpDisplay=="help"):    #if the input string is just help, display following message
                 print("This is a Python-docx based program that will allow the user to specify a .docx formatted file, parse that file, and save the parsed XML information to a specific location. The follwing are help commands; help, help input, help parse, help outname, help outloc, and help exit")
-            elif("help" in HelpDisplay):    #if it contains help and another word
-                if(len(HelpDisplay.split())==2):    #then it outputs a message depending on that other word
-                    if("outname" in HelpDisplay):    #if the second word contains outname, display following message
+            elif("help" in HelpDisplay):    #if it contains help and another string
+                if(len(HelpDisplay.split())==2):    #then it outputs a message depending on that other string
+                    if("outname" in HelpDisplay):    #if the second string is outname, display following message
                         print("This command will then allow the user to specify the name for the saved XML file. Typing outname will result in the program prompting the user to type in a specific, valid file name that the XML will be saved as.")
-                    elif("outloc" in HelpDisplay):    #if the second word contains outloc, display following message
+                    elif("outloc" in HelpDisplay):    #if the second string is outloc, display following message
                         print("This command will then allow the user to specify the location in which the XML will be saved. Typing outloc will result in the program prompting the user to type in a specific, valid folder location in their system for the XML to be saved within.")
-                    elif("input" in HelpDisplay):    #if the second word contains input, display following message
+                    elif("input" in HelpDisplay):    #if the second string is input, display following message
                         print("This command will then allow the user to specify the .docx file to be parsed. Typing input will result in the program prompting the user to enter a specific, valid filepath to the .docx file to be parsed.")
-                    elif("parse" in HelpDisplay):    #if the second word contains parse, display following message
+                    elif("parse" in HelpDisplay):    #if the second string is parse, display following message
                         print("This command will then parse the specified .docx file. Typing parse will result in the program running through its parse script, parsing the .docx file into its XML equivalent.")
-                    elif("exit" in HelpDisplay):    #if the second word contains exit, display following message
+                    elif("exit" in HelpDisplay):    #if the second string is exit, display following message
                         print("This command will then return the user to a previous menu, or stop the program.")
-                    else:    #if the second word is not one of the valid help commands, print following error
+                    else:    #if the second string is not one of the valid second string options, print following error
                         print("Invalid command")
-                else:    #if the first word is not a valid command, print following error
+                else:    #if the first string is not a valid command, print following error
                     print("Invalid command")
             elif(HelpDisplay=="exit"):    #if the user inputs exit, exits program
                 exit()
